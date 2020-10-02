bb2ee8bd0c3959f81da84a1d0db7a1b899fef558
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-02 01:20:45 -0400

    Foreach

--- cSharp_fundamentals/Program.cs
+++ cSharp_fundamentals/Program.cs
@@ -6,25 +6,11 @@ namespace cSharp_fundamentals
     {
         static void Main(string[] args)
         {
-            //Casting
-           
-            double pi = 3.14;
-            int intpi = (int)pi;
-
-            double num1 = 15;
-            double num2 = 10.5;
-
-            //Floor
-            Console.WriteLine(Math.Floor(num2));
-
-            //Same as Max, Min, Round, Sqrt, Pow, Ceiling, Abs,
-
-            //Random number
-            Random rand = new Random();
-            Console.WriteLine("Random number between 1 - 10");
-            for(int i = 0; i < 10; i++)
+            //foreach
+            string randomS = "This is a beautiful day!";
+            foreach(char c in randomS)
             {
-                Console.WriteLine(rand.Next(1, 11));
+                Console.WriteLine(c);
             }
 
         }
