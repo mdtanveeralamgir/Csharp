f3988e45704473ac26229d3dba5bccf0ed96a2bc
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-02 00:28:12 -0400

    First Commit

---
+++ cSharp_fundamentals/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cSharp_fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
