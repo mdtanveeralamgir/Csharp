f62c5e0a650e5a6d94622a18a851b545cbd50c3d
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-02 00:53:02 -0400

    Premative type with auto variables

--- cSharp_fundamentals/Program.cs
+++ cSharp_fundamentals/Program.cs
@@ -6,9 +6,46 @@ namespace cSharp_fundamentals
     {
         static void Main(string[] args)
         {
-            Console.Write("What's your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+            /*
+            //write on console
+            Console.Write("What's your name?"); //No new line
+            string name = Console.ReadLine(); //read from console
+            Console.WriteLine("Hello " + name); //With new line
+            */
+
+            //**********************************************************
+
+            //Premative types and values
+
+            //Integer
+            int maxInt = int.MaxValue;
+            Console.WriteLine(maxInt); //2147483647
+
+            long maxLong = long.MaxValue;
+            Console.WriteLine(maxLong); //9223372036854775807
+
+            //Decimal
+            decimal maxDeci = decimal.MaxValue;
+            Console.WriteLine(maxDeci); //79228162514264337593543950335
+
+            //Float
+            float maxFloat = float.MaxValue;
+            Console.WriteLine(maxFloat); //3.4028235E+38
+
+            //Double
+            double maxDouble = double.MaxValue;
+            Console.WriteLine(maxDouble); //1.7976931348623157E+308
+
+            //Variable on the fly. similar to auto in c++
+            var cPlusPlusAuto = "Tanveer";
+            Console.WriteLine(cPlusPlusAuto);
+            //cPlusPlusAuto = 2; //Error
+
+            //Get the type of a data
+            Console.WriteLine("cPlusPlusAuto is a {0}", cPlusPlusAuto.GetTypeCode());
+
+
+
         }
     }
 }
