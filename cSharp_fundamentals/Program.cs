de7553d2c09479c6bcbcc25cbd986984f6d0084e
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-02 23:50:18 -0400

    List

--- cSharp_fundamentals/Program.cs
+++ cSharp_fundamentals/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
+
 
 namespace cSharp_fundamentals
 {
@@ -8,32 +10,18 @@ namespace cSharp_fundamentals
         static void Main(string[] args)
         {
 
-            //Array
-            int[] randNumArray;
-            int[] randArray = new int[5];
-            int[] randomArray = { 1, 2, 3, 4, 5 };
-            foreach(var value  in randomArray)
+            //List
+
+            List<int> list = new List<int>();
+            list.Add(2);
+
+            //Add array into list
+            int[] numArray = { 1, 2, 3, 4 };
+            list.AddRange(numArray);
+
+            for(int i = 0; i < list.Count; i++)
             {
-                //Console.WriteLine(value);
-            }
-
-            //Console.WriteLine(Array.IndexOf(randomArray, 1));
-
-            //multidimantional array
-            int[,] multiArray = new int[5, 3];
-            int[,] multiArray2 = { { 0, 1 }, { 2, 1 }, { 3, 2 } };
-
-            foreach(var value in multiArray2)
-            {
-                //Console.WriteLine(value);
-            }
-
-            for(int x = 0; x < multiArray2.GetLength(0); x++)
-            {
-                for(int y = 0; y< multiArray2.GetLength(1); y++)
-                {
-                    Console.WriteLine("{0} | {1} : {2}", x, y, multiArray2[x,y]);
-                }
+                Console.WriteLine(list[i]);
             }
         }
     }
