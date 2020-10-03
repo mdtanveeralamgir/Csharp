19ed5aa0c9696a5aa2bdddf13fe1ba011f2c912b
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-02 23:41:17 -0400

    Array

--- cSharp_fundamentals/Program.cs
+++ cSharp_fundamentals/Program.cs
@@ -8,13 +8,33 @@ namespace cSharp_fundamentals
         static void Main(string[] args)
         {
 
-            //String builder
-            StringBuilder sb = new StringBuilder();
-            sb.Append("This is the first sentense.");
-            sb.AppendFormat("My Name is {0} and I live in {1}", "Opel", "Canada");
-            sb.Replace("a", "e");
-            Console.WriteLine(sb.ToString());
+            //Array
+            int[] randNumArray;
+            int[] randArray = new int[5];
+            int[] randomArray = { 1, 2, 3, 4, 5 };
+            foreach(var value  in randomArray)
+            {
+                //Console.WriteLine(value);
+            }
 
+            //Console.WriteLine(Array.IndexOf(randomArray, 1));
+
+            //multidimantional array
+            int[,] multiArray = new int[5, 3];
+            int[,] multiArray2 = { { 0, 1 }, { 2, 1 }, { 3, 2 } };
+
+            foreach(var value in multiArray2)
+            {
+                //Console.WriteLine(value);
+            }
+
+            for(int x = 0; x < multiArray2.GetLength(0); x++)
+            {
+                for(int y = 0; y< multiArray2.GetLength(1); y++)
+                {
+                    Console.WriteLine("{0} | {1} : {2}", x, y, multiArray2[x,y]);
+                }
+            }
         }
     }
 }
