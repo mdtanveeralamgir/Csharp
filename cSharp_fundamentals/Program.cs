ae5670ad167d3e77cddafdd660243a9659af8c80
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-06 02:06:21 -0400

    full lambda

--- cSharp_fundamentals/Program.cs
+++ cSharp_fundamentals/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 
@@ -18,8 +19,19 @@ namespace cSharp_fundamentals
             //func<param1, param2, returnt type>
             Func<int, int, bool> isEqual = (x, y) => x > y;
 
-            Console.WriteLine(isEqual(10, 9));
-           
+            Console.WriteLine(isEqual.Invoke(10, 9));
+
+            //Appling lambda on a list
+            List<int> fullList = new List<int> { 1, 2, 3, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Filtering out the odd numbers and store it in a new list
+            List<int> oddList = fullList.Where(n=>n%2!=0).ToList();
+
+            foreach(int value in oddList)
+            {
+                Console.WriteLine(value);
+            }
+
         }
     }
 }
